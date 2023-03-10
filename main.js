3df0b33f76954e0f33335635404c05fd19629863
keshavkumar23 <102866232+keshavkumar23@users.noreply.github.com>
2023-03-10 19:20:03 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+const input = document.querySelector('input');
+const addTask = document.querySelector('.Input > button');
+
+addTask.addEventListener('click', AddTask);
+
+function AddTask(e){
+
+    const incompletedTask = document.querySelector('.IncompletedTask');
+    const completedTask = document.querySelector('.CompletedTask');
+
+    const newTask = document.createElement('li');
+    const markTask = document.createElement('button');
+    const deleteTask = document.createElement('button');
+
+    markTask.innerHTML = 'Mark';
+    deleteTask.innerHTML = 'Del';
+
+    if(input.value != ''){
+        newTask.textContent = input.value;
+        input.value = '';
+        incompletedTask.appendChild(newTask);
+        newTask.appendChild(markTask);
+        newTask.appendChild(deleteTask);
+    }
+
+    markTask.addEventListener('click', function(){
+        const current = this.parentNode;
+        current.remove();
+        completedTask.appendChild(current);
+        markTask.style.display = 'none';
+    });
+
+    deleteTask.addEventListener('click', function(){
+        const current = this.parentNode;
+        current.remove();
+    });
+}
